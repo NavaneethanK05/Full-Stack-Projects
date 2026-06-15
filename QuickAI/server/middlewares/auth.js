8e9b552be8afc1049c7387fa957af9474e9c4aef
Navaneethan05K <you@example.com>
2026-06-15 19:32:45 +0530

    Added all projects

---
+++ QuickAI/server/middlewares/auth.js
@@ -0,0 +1,71 @@
+import { clerkClient } from "@clerk/express";
+
+// Middleware to check userId and hasPremiumPlan
+
+export const auth = async (req, res, next)=>{
+    try {
+        
+        const {userId, has} = await req.auth();
+        const hasPremiumPlan = await has({plan: 'premium'});
+
+        const user = await clerkClient.users.getUser(userId);
+
+        if(!hasPremiumPlan && user.privateMetadata.free_usage){
+            req.free_usage = user.privateMetadata.free_usage
+        } else{
+            await clerkClient.users.updateUserMetadata(userId, {
+                privateMetadata: {
+                    free_usage: 0
+                }
+            })
+            req.free_usage = 0;
+        }
+
+        req.plan = hasPremiumPlan ? 'premium' : 'free';
+        next()
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+
+// import { clerkClient, getAuth } from "@clerk/express";
+
+// export const auth = async (req, res, next) => {
+//   try {
+//     const { userId, has } = getAuth(req);
+
+//     if (!userId) {
+//       return res.status(401).json({
+//         success: false,
+//         message: "Unauthorized",
+//       });
+//     }
+
+//     const hasPremiumPlan = has ? has({ plan: "premium" }) : false;
+
+//     const user = await clerkClient.users.getUser(userId);
+
+//     if (!hasPremiumPlan && user.privateMetadata.free_usage) {
+//       req.free_usage = user.privateMetadata.free_usage;
+//     } else {
+//       await clerkClient.users.updateUserMetadata(userId, {
+//         privateMetadata: {
+//           free_usage: 0,
+//         },
+//       });
+
+//       req.free_usage = 0;
+//     }
+
+//     req.userId = userId;
+//     req.plan = hasPremiumPlan ? "premium" : "free";
+
+//     next();
+//   } catch (error) {
+//     return res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
